4ed22d4536fc09d48086c45f99f8ae8c3a9076d2
Bohdan <yurchenko.b.l@gmail.com>
2021-04-19 11:40:29 +0300

    cleared the code

--- Adv_HW#10/js/main.js
+++ Adv_HW#10/js/main.js
@@ -4,7 +4,6 @@ const main = document.body;
 main.classList.add('main');
 main.style.backgroundColor = '#242424';
 main.style.boxSizing = 'border-box';
-
 const title =
   `<h2 class="tile"
     style="color:bisque;
@@ -13,11 +12,8 @@ const title =
     Adv_HW#10 "DOM"
   </h2>`;
 main.insertAdjacentHTML('afterbegin', title);
-
-
 const generateColor = () =>
   '#' + Math.floor(Math.random() * 16777215).toString(16);
-
 
 function generateBlocks() {
   let idItem = 0;
@@ -27,8 +23,7 @@ function generateBlocks() {
   btnBlink.setAttribute('style', "display:inline-block");
   const box =
     `<div class="box"
-      style="
-      display: flex;
+      style="display: flex;
       flex-wrap: wrap;
       margin: 50px auto;
       width: 250px;
@@ -40,9 +35,8 @@ function generateBlocks() {
   for (let i = 1; i <= 25; i++) {
     idItem = idItem++;
     container.insertAdjacentHTML('beforeend',
-      `<div class="box__item" data-id="box__item-${idItem}"
-        style="
-        background-color:${generateColor()};
+      `<div class="box__item"
+        style="background-color:${generateColor()};
         width: 50px;
         height: 50px;
         display: inline-block;">
@@ -51,14 +45,11 @@ function generateBlocks() {
   }
 }
 
-const showBlocksBtn = `
-<button id="show_blocks">Show blocks</button>`;
+const showBlocksBtn = `<button id="show_blocks">Show blocks</button>`;
 main.insertAdjacentHTML('afterbegin', showBlocksBtn);
 document.getElementById('show_blocks').addEventListener('click', generateBlocks);
 const showBtn = document.getElementById('show_blocks');
-
-const blinkBlocksBtn = `
-<button style="display:none" id="blink_blocks">Blink blocks</button>`;
+const blinkBlocksBtn = `<button style="display:none" id="blink_blocks">Blink blocks</button>`;
 showBtn.insertAdjacentHTML('afterend', blinkBlocksBtn);
 document.getElementById('blink_blocks').addEventListener('click', setIntervalBlink);
 
@@ -68,5 +59,5 @@ function changeColorBG() {
 }
 
 function setIntervalBlink(time = 1000) {
-setInterval(()=>changeColorBG(), 1000);
+  setInterval(() => changeColorBG(), 1000);
 }
