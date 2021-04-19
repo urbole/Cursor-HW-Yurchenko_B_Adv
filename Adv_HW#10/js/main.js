2c8a7ed57d03778c4b7789e0e4f5e0ab2e53128e
Bohdan <yurchenko.b.l@gmail.com>
2021-04-19 11:23:04 +0300

    mdifted code and added blinking

--- Adv_HW#10/js/main.js
+++ Adv_HW#10/js/main.js
@@ -5,19 +5,6 @@ main.classList.add('main');
 main.style.backgroundColor = '#242424';
 main.style.boxSizing = 'border-box';
 
-const box =
-  `<div class="box"
-    style="
-    display: flex;
-    flex-wrap: wrap;
-    margin: 50px auto;
-    width: 250px;
-    height: 250px;">
-  </div>`;
-main.insertAdjacentHTML('afterbegin', box);
-
-const container = document.querySelector('.box');
-
 const title =
   `<h2 class="tile"
     style="color:bisque;
@@ -27,25 +14,59 @@ const title =
   </h2>`;
 main.insertAdjacentHTML('afterbegin', title);
 
+
 const generateColor = () =>
- '#' + Math.floor(Math.random() * 16777215).toString(16);
+  '#' + Math.floor(Math.random() * 16777215).toString(16);
 
 
 function generateBlocks() {
   let idItem = 0;
+  const btnDisabled = document.getElementById('show_blocks');
+  btnDisabled.setAttribute("disabled", "disabled");
+  const btnBlink = document.getElementById('blink_blocks');
+  btnBlink.setAttribute('style', "display:inline-block");
+  const box =
+    `<div class="box"
+      style="
+      display: flex;
+      flex-wrap: wrap;
+      margin: 50px auto;
+      width: 250px;
+      height: 250px;">
+    </div>`;
+  main.insertAdjacentHTML('afterend', box);
+  const container = document.querySelector('.box');
 
   for (let i = 1; i <= 25; i++) {
-    idItem = ++idItem;
+    idItem = idItem++;
     container.insertAdjacentHTML('beforeend',
       `<div class="box__item" data-id="box__item-${idItem}"
-      style="
-      background-color:${generateColor()};
-      width: 50px;
-      height: 50px;
-      display: inline-block;
-      "></div>`
+        style="
+        background-color:${generateColor()};
+        width: 50px;
+        height: 50px;
+        display: inline-block;">
+      </div>`
     );
   }
 }
 
-generateBlocks();
+const showBlocksBtn = `
+<button id="show_blocks">Show blocks</button>`;
+main.insertAdjacentHTML('afterbegin', showBlocksBtn);
+document.getElementById('show_blocks').addEventListener('click', generateBlocks);
+const showBtn = document.getElementById('show_blocks');
+
+const blinkBlocksBtn = `
+<button style="display:none" id="blink_blocks">Blink blocks</button>`;
+showBtn.insertAdjacentHTML('afterend', blinkBlocksBtn);
+document.getElementById('blink_blocks').addEventListener('click', setIntervalBlink);
+
+function changeColorBG() {
+  const blockItem = document.querySelectorAll('.box__item');
+  blockItem.forEach(item => item.style.backgroundColor = generateColor());
+}
+
+function setIntervalBlink(time = 1000) {
+setInterval(()=>changeColorBG(), 1000);
+}
